ff861222001511738572c29d04c2c0be2507f2d8
brucekaushik <brucekaushik>
2019-04-29 07:20:51 +0530

    remove exit

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -863,7 +863,7 @@ server = None
 def load_server_wide_modules():
     print("contenthive: modules about to get loaded = ", odoo.conf.server_wide_modules)
     print("contenthive: we wonder why web is already loaded...")
-    exit()
+
     for m in odoo.conf.server_wide_modules:
         try:
             odoo.modules.module.load_openerp_module(m)
